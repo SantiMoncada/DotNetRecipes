559f36005fe1e90028296db47d11bbcbdc327419
Santiago Moncada <90731161+SantiMoncada@users.noreply.github.com>
2022-09-20 17:11:58 +0200

    Basic CRUD done

--- BuberBreakfast/Controllers/BreakfastController.cs
+++ BuberBreakfast/Controllers/BreakfastController.cs
@@ -54,18 +54,44 @@ public class BreakfastController : ControllerBase
     [HttpGet("{id:guid}")]
     public IActionResult GetBreakfast(Guid id)
     {
-        return Ok(id);
+        Breakfast breakfast = _breakfastService.GetBreakfast(id);
+
+        var response = new BreakfastResponse(
+            breakfast.Id,
+            breakfast.Name,
+            breakfast.Description,
+            breakfast.StartDateTime,
+            breakfast.EndDateTime,
+            breakfast.LastModifiedDateTime,
+            breakfast.Savory,
+            breakfast.Sweet
+        );
+        return Ok(response);
     }
 
     [HttpPut("{id:guid}")]
     public IActionResult UpsertBreakfast(Guid id, CreateBreakfastRequest request)
     {
-        return Ok(request);
+        var breakfast = new Breakfast(
+            id,
+            request.Name,
+            request.Description,
+            request.StartDateTime,
+            request.EndDateTime,
+            DateTime.UtcNow,
+            request.Savory,
+            request.Sweet
+        );
+
+        _breakfastService.UpsertBreakfast(breakfast);
+        //return 201 if a new breakfast wasa created
+        return NoContent();
     }
 
     [HttpDelete("{id:guid}")]
     public IActionResult DeleteBreakfast(Guid id)
     {
-        return Ok(id);
+        _breakfastService.DeleteBreakfast(id);
+        return NoContent();
     }
 }
--- BuberBreakfast/Services/Breakfasts/IBreakfastService.cs
+++ BuberBreakfast/Services/Breakfasts/IBreakfastService.cs
@@ -4,4 +4,7 @@ using BuberBreakfast.Models;
 public interface IBreakfastService
 {
     void CreateBreakfast(Breakfast request);
+    Breakfast GetBreakfast(Guid id);
+    void UpsertBreakfast(Breakfast breakfast);
+    void DeleteBreakfast(Guid id);
 }
